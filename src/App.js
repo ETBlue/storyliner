7440ad748aab9f6a6db0c140ba502c4a34a8966e
ETBlue <etblue.tw@gmail.com>
2018-04-05 22:17:05 +0800

    add timeline ui

--- src/App.js
+++ src/App.js
@@ -20,9 +20,9 @@ class App extends Component {
 
     const csv2Obj = (text) => {
       const lines = text.split('\n')
-      const headers = lines[0].split(',')
+      const headers = lines[1].split(',')
       let result = []
-      for (let i = 1; i < lines.length; i++){
+      for (let i = 2; i < lines.length; i++){
         let obj = {}
         const columns = lines[i].split(',')
         headers.forEach((header, index) => {
@@ -33,44 +33,111 @@ class App extends Component {
       return result
     }
 
-    fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ8ukLhLNcPLc20_7J2ju6_e_KSLW2RW0LDu_1_4__IvaVUCO1BhZ9RGwefcWkOVRQ8XjlYv6MSe8oA/pub?gid=65178148&single=true&output=csv")
-      .then((respond) => {
-        respond.text().then(text => {
-          this.setState({
-            title: text.split('\n')[1].split(',')[0]
-          })
-        })
-      })
-      .catch(error => console.error(error))
+    const csv2Title = (text) => {
+      return text.split('\n')[0].split(',')[0]
+    }
 
     fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vQ8ukLhLNcPLc20_7J2ju6_e_KSLW2RW0LDu_1_4__IvaVUCO1BhZ9RGwefcWkOVRQ8XjlYv6MSe8oA/pub?gid=1467627135&single=true&output=csv")
       .then((respond) => {
         respond.text().then(text => {
           this.setState({
+            title: csv2Title(text),
             data: csv2Obj(text),
             isLoaded: true
           })
         })
       })
       .catch(error => console.error(error))
+
   }
 
   render() {
 
+    const renderQuote = (text, author) => {
+      if (text.length === 0) {
+        return
+      }
+      const quotes = text.split(';').map((content, index) => (
+        <blockquote key={index} >
+          <i className='quote left icon' />
+          <i className='quote right icon' />
+          {content}<span style={{fontSize: '0.85em', opacity: '0.85', display:'inline-block'}} > — {author}</span>
+        </blockquote>
+      ))
+      return(
+        <div className='ui secondary segment'>
+          {quotes}
+        </div>
+      )
+    }
+
+    const Menu = this.state.data.map((content, index) => (
+      <a key={index} className='item'>
+        {content.date}
+      </a>
+    ))
+
     const Relation = this.state.data.map((content, index) => (
-        <p key={index}>
-          {content._subject} {content.action} {content._object}
-        </p>
+      <article key={index}>
+      <div className='ui two column stackable grid' >
+        <div className='eleven wide column'>
+        <div className='ui segments'>
+          <div className='ui segment'>
+            <p>
+              <a className='ui large horizontal label' data-role={content._subject}>
+                {content._subject}
+              </a>
+              <span>
+              {content.action}
+              </span>
+              <a className='ui large horizontal label' data-role={content._object}>
+                {content._object}
+              </a>
+              <span>
+              {content.content_topic}
+              </span>
+            </p>
+            <p style={{marginTop: '-0.5rem', opacity: '0.85', fontSize: '0.85em'}} >
+              {content.via}{content.channel}{content.content_carrier} — 
+              <a href={content["ref_url"]} target='_blank'>
+              {content.ref_title.length > 0 ?
+                content.ref_title : content.ref_url}
+              </a>
+            </p>
+          </div>
+          {renderQuote(content.quote_content, content._quote_author)}
+        </div>
+        </div>
+        <div className='five wide column'>
+          <h4 className='ui dividing header' style={{color: 'rgba(0,0,0,0.6)'}} >
+            圍觀筆記
+          </h4>
+          <p style={{opacity: '0.85'}} >
+          {content.note}
+          </p>
+        </div>
+      </div>
+      </article>
     ))
 
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{this.state.title}</h1>
+        <header className='App-header'>
+          <h1 className="ui header">
+            <img src={logo} alt="logo" className='ui image App-logo' />
+            <div className='content'>
+              <i className='icon bars' style={{position: 'absolute', right: '2rem', margin: '0', opacity: '0.5', fontSize: '1.5rem'}} />
+              {this.state.title}
+            </div>
+          </h1>
         </header>
-        <section className="App-intro">
+        <section className="App-body ui container" style={{display: 'flex'}} >
+          <nav className='ui vertical secondary pointing menu' style={{width: '5rem', flex: 'none'}}>
+          {Menu}
+          </nav>
+          <div style={{width: '100%', paddingLeft: '2rem'}} >
           {Relation}
+          </div>
         </section>
       </div>
     );
