6f9253dd9d4357a778397f6ed80a7680b3fa4325
ETBlue <etblue.tw@gmail.com>
2019-04-19 04:07:42 +0800

    Updates

---
+++ precache-manifest.96315177b826e156aa00531fd4b91a82.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/storyliner/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "a829c938d97a053c8d6a",
+    "url": "/storyliner/static/css/main.ff3d4bb6.chunk.css"
+  },
+  {
+    "revision": "db8a2d967d8818ed8f76",
+    "url": "/storyliner/static/js/runtime~main.2f6f50a4.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/storyliner/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "f1c9751cbc57bf80232e",
+    "url": "/storyliner/static/js/2.b20b654b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/storyliner/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/storyliner/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/storyliner/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/storyliner/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/storyliner/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/storyliner/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "a829c938d97a053c8d6a",
+    "url": "/storyliner/static/js/main.40d0a490.chunk.js"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/storyliner/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/storyliner/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/storyliner/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/storyliner/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/storyliner/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/storyliner/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/storyliner/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/storyliner/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/storyliner/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "f1c9751cbc57bf80232e",
+    "url": "/storyliner/static/css/2.84a727ca.chunk.css"
+  },
+  {
+    "revision": "76b3b60fcedd5fabfc84e85141298def",
+    "url": "/storyliner/index.html"
+  }
+];
