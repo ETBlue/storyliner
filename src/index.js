27bd60fe53c7a17d27ed4d6b08d07d95c87fe224
ETBlue <etblue.tw@gmail.com>
2019-04-19 19:51:08 +0800

    [env] seperate semantic ui css for better debugging experience

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter as Router} from 'react-router-dom'
-import 'semantic-ui-css/semantic.min.css'
 
 import {SETTINGS} from './_shared'
 import {App} from './App/'
