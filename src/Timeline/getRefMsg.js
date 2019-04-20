c6a3a31af2f93d3f495b3192711496e427f792dd
ETBlue <etblue.tw@gmail.com>
2019-04-21 04:36:12 +0800

    [bug fix] handle same day cases

--- src/Timeline/getRefMsg.js
+++ src/Timeline/getRefMsg.js
@@ -1,5 +1,12 @@
 export default ({event, refEvent, refEventTitle}) => {
   const msg = event.momentDate.to(refEvent.momentDate, true)
-  const prep = event.momentDate.isBefore(refEvent.momentDate) ? 'before' : 'after'
-  return `${msg} ${prep} ${refEventTitle}`
+  let timeTo
+  if (event.momentDate.isBefore(refEvent.momentDate)) {
+    timeTo = `${msg} before`
+  } else if (event.momentDate.isAfter(refEvent.momentDate)) {
+    timeTo = `${msg} after`
+  } else {
+    timeTo = 'same day as'
+  }
+  return `${timeTo} ${refEventTitle}`
 }
