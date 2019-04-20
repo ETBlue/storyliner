d829d96eca425826590a0f8bde1ab2233bd34015
ETBlue <etblue.tw@gmail.com>
2019-04-21 02:47:25 +0800

    Updates

---
+++ precache-manifest.24c99ed7282d1171dffbbe36905658d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/storyliner/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "db8a2d967d8818ed8f76",
+    "url": "/storyliner/static/js/runtime~main.2f6f50a4.js"
+  },
+  {
+    "revision": "b363db9535bf68a0d403",
+    "url": "/storyliner/static/js/main.850dfde6.chunk.js"
+  },
+  {
+    "revision": "974f3ff4f7927a6a546a",
+    "url": "/storyliner/static/js/2.3e10c5ef.chunk.js"
+  },
+  {
+    "revision": "b363db9535bf68a0d403",
+    "url": "/storyliner/static/css/main.804b6877.chunk.css"
+  },
+  {
+    "revision": "1b9f54e961130348a17c77f44aa38de0",
+    "url": "/storyliner/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/storyliner/precache-manifest.9eac8830ca05164167eb331d62f374bc.js"
+  "/storyliner/precache-manifest.24c99ed7282d1171dffbbe36905658d5.js"
 );
 
 workbox.clientsClaim();
