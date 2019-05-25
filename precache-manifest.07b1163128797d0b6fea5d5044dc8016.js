ec34a4c2217b0d6ec367f270260739ecefa7ed32
ETBlue <etblue.tw@gmail.com>
2019-05-25 20:17:20 +0800

    Updates

---
+++ precache-manifest.07b1163128797d0b6fea5d5044dc8016.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f57a17cdcecdd0823e51db98b9c0506",
+    "url": "/storyliner/index.html"
+  },
+  {
+    "revision": "7b46c91e7342510f586d",
+    "url": "/storyliner/static/css/main.32e0a8d0.chunk.css"
+  },
+  {
+    "revision": "7ce8db5bb40a00dc8125",
+    "url": "/storyliner/static/js/2.4333b01f.chunk.js"
+  },
+  {
+    "revision": "7b46c91e7342510f586d",
+    "url": "/storyliner/static/js/main.56573f9a.chunk.js"
+  },
+  {
+    "revision": "db8a2d967d8818ed8f76",
+    "url": "/storyliner/static/js/runtime~main.2f6f50a4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/storyliner/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/storyliner/precache-manifest.20517f69c51f71cb5275f51a183fdf83.js"
+  "/storyliner/precache-manifest.07b1163128797d0b6fea5d5044dc8016.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/storyliner/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/storyliner/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
